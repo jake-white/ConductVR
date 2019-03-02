f367cdf1be484cab7a0f81b7a47af2d207d2a87a
Jake White <squirrelboy1225@gmail.com>
2019-03-02 06:17:58 -0500

    SteamVR, basic pitch shifting setup

---
+++ Assets/Conductor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class Conductor : MonoBehaviour
+{
+    public Transform baton;
+    public AudioMixer mixer;
+    public Song song;
+    public AudioSource[] sources;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("ChangePitch", 2.0f, 0.3f);
+        sources = song.GetInstruments();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     
+
+    }
+
+    void ChangePitch() {
+        float pitch = baton.position.y;
+        foreach(AudioSource instrument in sources) {
+            instrument.pitch = pitch;
+        }
+        mixer.SetFloat("PitchShift", 1/pitch);
+    }
+}
