d664782ac15afdcc72f0427cc91192ab1be6d5ee
White <whitejaa@ads.iu.edu>
2019-04-12 12:03:10 -0400

    Working gaze detection

---
+++ Assets/Scripts/GazeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeController : MonoBehaviour
+{
+    public List<Instrument> symphony;
+    bool[] inGaze;
+    Camera cam;
+    int layer_mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inGaze = new bool[symphony.Count];
+        cam = Camera.main;
+        layer_mask = LayerMask.GetMask("Instruments");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < symphony.Count; ++i) {
+            inGaze[i] = false;
+        }
+        Ray ray = new Ray(cam.transform.position, cam.transform.rotation * Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, layer_mask))
+        {
+            if(hit.collider.GetComponent<Instrument>() != null) {
+                int indexInGaze = symphony.IndexOf(hit.collider.GetComponent<Instrument>());
+                inGaze[indexInGaze] = true;
+            }
+        }
+        else
+        {
+
+        }
+        
+        for(int i = 0; i < symphony.Count; ++i) {
+            if(inGaze[i]) {
+                symphony[i].GazeEnter();
+            }
+            else {
+                symphony[i].GazeExit();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Instrument.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrument : MonoBehaviour
+{
+    public Material selected, unselected;
+    bool inGaze;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().material = unselected;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GazeEnter() {
+        if(!inGaze) {
+            GetComponent<MeshRenderer>().material = selected;
+        }
+        inGaze = true;
+    }
+
+    public void GazeExit() {
+        if(inGaze) {
+            GetComponent<MeshRenderer>().material = unselected;
+        }
+        inGaze = false;
+    }
+}
